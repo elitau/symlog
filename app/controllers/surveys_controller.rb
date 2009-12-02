d13fb6d0b9032a4e8259604476a60445516eba88
Eduard Litau <eduard.litau@gmail.com>
2009-12-02 08:47:09 +0100

    CHANGED Styling

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -2,7 +2,7 @@ class SurveysController < ApplicationController
   # GET /surveys
   # GET /surveys.xml
   def index
-    @surveys = Survey.find_all
+    # @surveys = Survey.find_all
 
     respond_to do |format|
       format.html # index.html.erb
@@ -34,9 +34,9 @@ class SurveysController < ApplicationController
   end
 
   # GET /surveys/1/edit
-  def edit
-    @survey = Survey.find(params[:id])
-  end
+  # def edit
+  #   @survey = Survey.find(params[:id])
+  # end
 
   # POST /surveys
   # POST /surveys.xml
@@ -58,30 +58,30 @@ class SurveysController < ApplicationController
 
   # PUT /surveys/1
   # PUT /surveys/1.xml
-  def update
-    @survey = Survey.find(params[:id])
-
-    respond_to do |format|
-      if @survey.update_attributes(params[:survey])
-        flash[:notice] = 'Survey was successfully updated.'
-        format.html { redirect_to(@survey) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @survey.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /surveys/1
-  # DELETE /surveys/1.xml
-  def destroy
-    @survey = Survey.find(params[:id])
-    @survey.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(surveys_url) }
-      format.xml  { head :ok }
-    end
-  end
+  # def update
+  #   @survey = Survey.find(params[:id])
+  # 
+  #   respond_to do |format|
+  #     if @survey.update_attributes(params[:survey])
+  #       flash[:notice] = 'Survey was successfully updated.'
+  #       format.html { redirect_to(@survey) }
+  #       format.xml  { head :ok }
+  #     else
+  #       format.html { render :action => "edit" }
+  #       format.xml  { render :xml => @survey.errors, :status => :unprocessable_entity }
+  #     end
+  #   end
+  # end
+  # 
+  # # DELETE /surveys/1
+  # # DELETE /surveys/1.xml
+  # def destroy
+  #   @survey = Survey.find(params[:id])
+  #   @survey.destroy
+  # 
+  #   respond_to do |format|
+  #     format.html { redirect_to(surveys_url) }
+  #     format.xml  { head :ok }
+  #   end
+  # end
 end
